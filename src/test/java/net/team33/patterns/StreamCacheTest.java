cce758aabe87af915984075487acea7d12911de0
Andi Kluge <andi.kluge@mobi4friends.de>
2018-01-25 12:22:39 +0100

    new: StreamCache

---
+++ src/test/java/net/team33/patterns/StreamCacheTest.java
@@ -0,0 +1,50 @@
+package net.team33.patterns;
+
+import net.team33.reflect.FieldFilter;
+import net.team33.reflect.FieldStream;
+import net.team33.reflect.test.Sample;
+import net.team33.reflect.test.SampleEx;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StreamCacheTest {
+
+    @Test
+    public final void from() {
+        final StreamCache<Class<?>, Field> subject = StreamCache.builder(FieldStream.FLAT)
+                .build();
+        Assert.assertEquals(
+                FieldStream.FLAT.apply(Sample.class).collect(Collectors.toSet()),
+                subject.from(Sample.class).collect(Collectors.toSet())
+        );
+    }
+
+    @Test
+    public final void fromWithFilter() {
+        final StreamCache<Class<?>, Field> subject = StreamCache.builder(FieldStream.DEEP)
+                .addFilter(FieldFilter.INSTANCE)
+                .addFilter(FieldFilter.TRANSIENT.negate())
+                .addFilter(FieldFilter.PUBLIC)
+                .build();
+        final Set<Field> expected = FieldStream.DEEP.apply(SampleEx.class)
+                .filter(FieldFilter.INSTANCE)
+                .filter(FieldFilter.TRANSIENT.negate())
+                .filter(FieldFilter.PUBLIC)
+                .collect(Collectors.toSet());
+        final Set<Field> result = subject.from(SampleEx.class)
+                .collect(Collectors.toSet());
+        Assert.assertEquals(
+                expected,
+                result
+        );
+        Assert.assertEquals(
+                result,
+                subject.from(SampleEx.class)
+                        .collect(Collectors.toSet())
+        );
+    }
+}
