e4df9d7d9829f2bdf792d94768cadedebe06b976
andi <akk-pg@team33.de>
2019-03-04 22:12:24 +0100

    debugged distance()

--- src/main/java/de/team33/libs/reflect/v3/Classes.java
+++ src/main/java/de/team33/libs/reflect/v3/Classes.java
@@ -1,79 +1,71 @@
 package de.team33.libs.reflect.v3;
 
-import java.util.Objects;
 import java.util.stream.Stream;
 
 
 /**
  * Utility for dealing with classes.
  */
-public class Classes
-{
+public class Classes {
 
-  private static final int MAX_DISTANCE = Short.MAX_VALUE;
+    private static final int MAX_DISTANCE = Short.MAX_VALUE;
 
-  public static int distance(final Class<?> subClass, final Class<?> superClass)
-  {
-    if (subClass == superClass)
-      return 0;
-    if (superClass.isAssignableFrom(subClass))
-      return 1 + distance(superClasses(subClass), superClass);
-    else
-      return MAX_DISTANCE;
-  }
+    public static int distance(final Class<?> subClass, final Class<?> superClass) {
+        if (subClass == superClass) {
+            return 0;
+        } else if (Object.class == superClass) {
+            return 1 + distance(subClass.getSuperclass(), superClass);
+        } else {
+            return 1 + distance(superClasses(subClass), superClass);
+        }
+    }
 
-  private static Stream<Class<?>> superClasses(final Class<?> subClass)
-  {
-    return (null == subClass) ? Stream.empty() :
-      Stream.concat(Stream.of(subClass.getInterfaces()), flat(subClass.getSuperclass()));
-  }
+    private static Stream<Class<?>> superClasses(final Class<?> subClass) {
+        return Stream.concat(Stream.of(subClass.getInterfaces()), flat(subClass.getSuperclass()));
+    }
 
-  private static int distance(final Stream<Class<?>> subClasses, final Class<?> superClass)
-  {
-    return subClasses.map(subClass -> distance(subClass, superClass))
-                     .reduce(MAX_DISTANCE, Math::min);
-  }
+    private static int distance(final Stream<Class<?>> subClasses, final Class<?> superClass) {
+        return subClasses
+                .filter(superClass::isAssignableFrom)
+                .map(subClass -> distance(subClass, superClass))
+                .reduce(Math::min)
+                .orElseThrow(() -> new IllegalStateException());
+    }
 
-  /**
-   * Streams a 'flat' class hierarchy (that focuses only on the specified class).
-   */
-  public static Stream<Class<?>> flat(final Class<?> subject)
-  {
-    return (null == subject) ? Stream.empty() : Stream.of(subject);
-  }
+    /**
+     * Streams a 'flat' class hierarchy (that focuses only on the specified class).
+     */
+    public static Stream<Class<?>> flat(final Class<?> subject) {
+        return (null == subject) ? Stream.empty() : Stream.of(subject);
+    }
 
-  /**
-   * Streams a class hierarchy that focuses on the specified class and its superclasses.
-   */
-  public static Stream<Class<?>> deep(final Class<?> subject)
-  {
-    return (null == subject) ? Stream.empty() :
-      Stream.concat(deep(subject.getSuperclass()), Stream.of(subject));
-  }
+    /**
+     * Streams a class hierarchy that focuses on the specified class and its superclasses.
+     */
+    public static Stream<Class<?>> deep(final Class<?> subject) {
+        return (null == subject) ? Stream.empty() :
+                Stream.concat(deep(subject.getSuperclass()), Stream.of(subject));
+    }
 
-  /**
-   * Streams a class hierarchy that focuses on the specified class, its superclasses and its superinterfaces.
-   */
-  public static Stream<Class<?>> wide(final Class<?> subject)
-  {
-    return broad(subject).distinct();
-  }
+    /**
+     * Streams a class hierarchy that focuses on the specified class, its superclasses and its superinterfaces.
+     */
+    public static Stream<Class<?>> wide(final Class<?> subject) {
+        return broad(subject).distinct();
+    }
 
-  private static Stream<Class<?>> broad(final Class<?> subject)
-  {
-    return (null == subject) ? Stream.empty() :
-      broad(subject.getInterfaces(), subject.getSuperclass(), subject);
-  }
+    private static Stream<Class<?>> broad(final Class<?> subject) {
+        return (null == subject) ? Stream.empty() :
+                broad(subject.getInterfaces(), subject.getSuperclass(), subject);
+    }
 
-  private static Stream<Class<?>> broad(final Class<?>[] interfaces,
-                                        final Class<?> superclass,
-                                        final Class<?> subject)
-  {
-    return Stream.concat(broad(interfaces), Stream.concat(broad(superclass), Stream.of(subject)));
-  }
+    private static Stream<Class<?>> broad(final Class<?>[] interfaces,
+                                          final Class<?> superclass,
+                                          final Class<?> subject) {
+        return Stream.concat(broad(interfaces), Stream.concat(broad(superclass), Stream.of(subject)));
+    }
 
-  private static Stream<Class<?>> broad(final Class<?>[] subjects)
-  {
-    return Stream.of(subjects).map(Classes::broad).reduce(Stream::concat).orElseGet(Stream::empty);
-  }
+    private static Stream<Class<?>> broad(final Class<?>[] subjects) {
+        return Stream.of(subjects).map(Classes::broad).reduce(Stream::concat).orElseGet(Stream::empty);
+    }
 }
--- src/test/java/de/team33/test/reflect/v3/ClassesTest.java
+++ src/test/java/de/team33/test/reflect/v3/ClassesTest.java
@@ -1,94 +1,114 @@
 package de.team33.test.reflect.v3;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
-
-import static org.junit.Assert.assertEquals;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import de.team33.libs.reflect.v3.Classes;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class ClassesTest
-{
-
-  @Test
-  public void distance()
-  {
-    assertEquals(0, Classes.distance(Inner.class, Inner.class));
-    assertEquals(1, Classes.distance(Inner.class, Super.class));
-    assertEquals(2, Classes.distance(Inner.class, Object.class));
-    assertEquals(1, Classes.distance(Inner.class, ISuper1.class));
-    assertEquals(1, Classes.distance(Inner.class, ISuper2.class));
-    assertEquals(-2, Classes.distance(Collection.class, Map.class));
-  }
-
-  @Test
-  public void empty()
-  {
-    Stream.<Function<Class<?>, Stream<Class<?>>>>of(Classes::flat,
-                                                    Classes::deep,
-                                                    Classes::wide).forEach(toStream -> assertEquals(
-      emptyList(),
-      toStream.apply(null).map(Class::toString).collect(Collectors.toList())));
-  }
-
-  @Test
-  public void flat()
-  {
-    assertEquals(singletonList("class de.team33.test.reflect.v3.ClassesTest$Inner"),
-                 Classes.flat(Inner.class).map(Class::toString).collect(Collectors.toList()));
-  }
-
-  @Test
-  public void deep()
-  {
-    assertEquals(Arrays.asList("class java.lang.Object",
-                               "class de.team33.test.reflect.v3.ClassesTest$Super",
-                               "class de.team33.test.reflect.v3.ClassesTest$Inner"),
-                 Classes.deep(Inner.class).map(Class::toString).collect(Collectors.toList()));
-  }
-
-  @Test
-  public void wide()
-  {
-    assertEquals(Arrays.asList("interface de.team33.test.reflect.v3.ClassesTest$ISuper1",
-                               "interface de.team33.test.reflect.v3.ClassesTest$ISuper2",
-                               "class java.lang.Object",
-                               "class de.team33.test.reflect.v3.ClassesTest$Super",
-                               "class de.team33.test.reflect.v3.ClassesTest$Inner"),
-                 Classes.wide(Inner.class).map(Class::toString).collect(Collectors.toList()));
-  }
-
-  private static class Inner extends Super implements ISuper1, ISuper2
-  {
-
-  }
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 
-  private interface ISuper1
-  {
+public class ClassesTest {
 
-  }
+    @Test
+    public void distance() {
+        assertEquals(0, Classes.distance(Inner.class, Inner.class));
+        assertEquals(1, Classes.distance(Inner.class, Super.class));
+        assertEquals(2, Classes.distance(Inner.class, Base.class));
+        assertEquals(3, Classes.distance(Inner.class, Object.class));
+        assertEquals(2, Classes.distance(Inner.class, ISuper1.class));
+        assertEquals(1, Classes.distance(Inner.class, ISuper2.class));
+        assertEquals(1, Classes.distance(ISuper2.class, ISuper1.class));
+        assertEquals(1, Classes.distance(Super.class, ISuper1.class));
+        assertEquals(3, Classes.distance(Inner.class, ISuper3.class));
+    }
 
+    @Test(expected = IllegalStateException.class)
+    public void distanceReverse() {
+        fail("Should fail but was " + Classes.distance(Super.class, Inner.class));
+    }
 
-  private interface ISuper2 extends ISuper1
-  {
+    @Test(expected = IllegalStateException.class)
+    public void distanceNonRelated() {
+        fail("Should fail but was " + Classes.distance(List.class, StringBuilder.class));
+    }
 
-  }
+    @Test(expected = NullPointerException.class)
+    public void distanceAnyNull() {
+        fail("Should fail but was " + Classes.distance(List.class, null));
+    }
 
+    @Test(expected = NullPointerException.class)
+    public void distanceNullAny() {
+        fail("Should fail but was " + Classes.distance(null, List.class));
+    }
 
-  private static class Super implements ISuper2
-  {
+    @Test
+    public void distanceNullNull() {
+        assertEquals(0, Classes.distance(null, null));
+    }
 
-  }
+    @Test
+    public void empty() {
+        Stream.<Function<Class<?>, Stream<Class<?>>>>of(Classes::flat,
+                Classes::deep,
+                Classes::wide).forEach(toStream -> assertEquals(
+                emptyList(),
+                toStream.apply(null).map(Class::toString).collect(Collectors.toList())));
+    }
+
+    @Test
+    public void flat() {
+        assertEquals(singletonList("class de.team33.test.reflect.v3.ClassesTest$Inner"),
+                Classes.flat(Inner.class).map(Class::toString).collect(Collectors.toList()));
+    }
+
+    @Test
+    public void deep() {
+        assertEquals(
+                Arrays.asList(
+                        "class java.lang.Object",
+                        "class de.team33.test.reflect.v3.ClassesTest$Base",
+                        "class de.team33.test.reflect.v3.ClassesTest$Super",
+                        "class de.team33.test.reflect.v3.ClassesTest$Inner"
+                ),
+                Classes.deep(Inner.class).map(Class::toString).collect(Collectors.toList()));
+    }
+
+    @Test
+    public void wide() {
+        assertEquals(Arrays.asList("interface de.team33.test.reflect.v3.ClassesTest$ISuper1",
+                "interface de.team33.test.reflect.v3.ClassesTest$ISuper2",
+                "interface de.team33.test.reflect.v3.ClassesTest$ISuper3",
+                "class java.lang.Object",
+                "class de.team33.test.reflect.v3.ClassesTest$Base",
+                "class de.team33.test.reflect.v3.ClassesTest$Super",
+                "class de.team33.test.reflect.v3.ClassesTest$Inner"),
+                Classes.wide(Inner.class).map(Class::toString).collect(Collectors.toList()));
+    }
+
+    private static class Inner extends Super implements ISuper2 {
+    }
+
+    private static class Super extends Base implements ISuper1 {
+    }
+
+    private static class Base implements ISuper3 {
+    }
+
+    private interface ISuper1 {
+    }
+
+    private interface ISuper2 extends ISuper1 {
+    }
+
+    private interface ISuper3 {
+    }
 }
